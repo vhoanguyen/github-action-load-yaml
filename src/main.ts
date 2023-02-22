72bec815cc64ad0e1950b466b1e83962b9890e97
Hoa Nguyen <hoanguyen@HNguyenMac.local>
2023-02-22 20:50:32 +1100

    2

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,26 @@
 const core = require("@actions/core");
 const github = require("@actions/github");
+const yaml = require('js-yaml')
+import * as fs from 'fs';
+import * as path from "path";
+
 
 function run(): void {
     const dataFileName = core.getInput("json_file");
-    console.log(dataFileName)
+    let config = readConfigFile(dataFileName)
+    core.setOutput("Data", config);
+
 }
+
+function readConfigFile(filePath: string) {
+    let fileContent = {}
+    try {
+      fileContent = yaml.load(fs.readFileSync(path.resolve(filePath), "utf8"));
+    }
+    catch {
+      return fileContent
+    }
+    return fileContent
+  }
+
 run()
